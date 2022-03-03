453a80bee5d6deda8f725001efd7a80be755322d
Raut Sir <Raut Sir@LAPTOP-UMPUNSIP>
2022-03-03 08:57:19 +0530

    hello

---
+++ src/B.java
@@ -0,0 +1,19 @@
+
+public class B 
+{ 
+	
+	public static void main(String[] args)
+	{   
+	 char c;
+	 
+		for(c='a';c<='z';c++)
+		{
+			System.out.print(c+" ");
+		}
+		System.out.print("\n");
+		for(c='z';c>='a';c--)
+		{
+			System.out.print(c+" ");
+		}
+	}
+}
